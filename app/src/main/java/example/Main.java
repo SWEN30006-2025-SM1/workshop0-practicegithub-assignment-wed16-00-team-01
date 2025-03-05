64810af7ec7ecfa25e21d5e168097722b1dae948
mpooley <mitchell.pooley@student.unimelb.edu.au>
2025-03-05 16:53:54 +1100

    Initial commit to generate workflow

--- app/src/main/java/example/Main.java
+++ app/src/main/java/example/Main.java
@@ -2,6 +2,7 @@ package example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello, World!");
         Calculator calculator = new Calculator();
         System.out.println("add = " + calculator.add(3, 5));
         System.out.println("minus = " + calculator.minus(8, 2));
