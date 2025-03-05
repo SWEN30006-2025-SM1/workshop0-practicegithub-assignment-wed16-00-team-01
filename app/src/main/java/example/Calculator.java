f594d07788e49e580ad7f238640032a18cd81c1a
Bec Chao <becchao@gmail.com>
2025-03-05 17:16:31 +1100

    Add multiplication and division

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -12,12 +12,10 @@ public class Calculator {
     }
 
     public int multiply(int a, int b) {
-        // TODO: Implement this method
-        return 10;
+        return a*b;
     }
 
     public double divide(int a, int b) {
-        // TODO: Implement this method
-        return 5;
+        return (double)a/b;
     }
 }
