15b839a0beef7e7ef55725c51b2c9f246bedc7ad
mpooley <mitchell.pooley@student.unimelb.edu.au>
2025-03-05 17:31:31 +1100

    Line changes

--- app/src/test/java/example/CalculatorTest.java
+++ app/src/test/java/example/CalculatorTest.java
@@ -18,17 +18,11 @@ public class CalculatorTest {
         Assert.assertEquals(10, calculator.minus(15, 5));
     }
     @Test
-    public void testMinusWithNegative() {
-        Assert.assertEquals(20, calculator.minus(15, -5));
-    }
+    public void testMinusWithNegative() { Assert.assertEquals(20, calculator.minus(15, -5)); }
     @Test
-    public void testMultiplyBasic() {
-        Assert.assertEquals(10, calculator.multiply(2, 5));
-    }
+    public void testMultiplyBasic() { Assert.assertEquals(10, calculator.multiply(2, 5)); }
     @Test
-    public void testMultiplyWithNegative() {
-        Assert.assertEquals(-15, calculator.multiply(15, -1));
-    }
+    public void testMultiplyWithNegative() { Assert.assertEquals(-15, calculator.multiply(15, -1)); }
     @Test
     public void testDivideBasic() {
         Assert.assertEquals(5, calculator.divide(10, 2), 0.5);
