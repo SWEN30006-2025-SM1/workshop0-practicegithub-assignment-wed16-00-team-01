4d24d573710cff99d9ab428a13423dacc16c430c
Bec Chao <becchao@gmail.com>
2025-03-05 17:26:20 +1100

    Add subtraction and multiplication tests

--- app/src/test/java/example/CalculatorTest.java
+++ app/src/test/java/example/CalculatorTest.java
@@ -18,8 +18,16 @@ public class CalculatorTest {
         Assert.assertEquals(10, calculator.minus(15, 5));
     }
     @Test
+    public void testMinusWithNegative() {
+        Assert.assertEquals(20, calculator.minus(15, -5));
+    }
+    @Test
     public void testMultiplyBasic() {
         Assert.assertEquals(10, calculator.multiply(2, 5));
+    }
+    @Test
+    public void testMultiplyWithNegative() {
+        Assert.assertEquals(-15, calculator.multiply(15, -1));
     }
     @Test
     public void testDivideBasic() {
